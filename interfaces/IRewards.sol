5c9dea483a5def141a404d6b7dbd3618273dcce0
Facu <fameal@gmail.com>
2021-07-06 00:00:37 +0200

    fix: another try to fix linter

--- interfaces/IRewards.sol
+++ interfaces/IRewards.sol
@@ -2,14 +2,23 @@
 pragma solidity 0.6.12;
 
 interface IRewards {
-  function notifyRewardAmount(uint256 reward) external;
-  function earned(address account) external view returns (uint256);
-  function stake(uint256 amount) external;
-  function withdraw(uint256 amount) external;
-  function exit() external;
-  function getReward() external;
-  function balanceOf(address account) external view returns (uint256);
-  function DURATION() external view returns (uint256);
-  function starttime() external view returns (uint256);
-  function periodFinish() external view returns (uint256);
+    function notifyRewardAmount(uint256 reward) external;
+
+    function earned(address account) external view returns (uint256);
+
+    function stake(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function exit() external;
+
+    function getReward() external;
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function DURATION() external view returns (uint256);
+
+    function starttime() external view returns (uint256);
+
+    function periodFinish() external view returns (uint256);
 }
--- interfaces/IUniswapV2Router.sol
+++ interfaces/IUniswapV2Router.sol
@@ -3,10 +3,10 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 interface IUniswapV2Router {
-  function getAmountsOut(
-        uint256 amountIn, 
-        address[] calldata path
-    ) external view returns (uint256[] memory amounts);
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
 
     function swapExactTokensForTokens(
         uint256 amountIn,
