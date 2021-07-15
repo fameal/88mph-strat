c284b8ede045a5091fadf0beab4aaa46b4d24f74
Facu <fameal@gmail.com>
2021-07-15 11:35:30 +0200

    fix: accounting comments

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -90,7 +90,6 @@ contract Strategy is BaseStrategy {
 
         uint256 assets = estimatedTotalAssets();
         uint256 debt = vault.strategies(address(this)).totalDebt;
-        uint256 balanceWant = balanceOfWant();
 
         if (assets > debt) {
             _profit = assets.sub(debt);
@@ -100,10 +99,21 @@ contract Strategy is BaseStrategy {
 
         uint256 amountToFree = _debtOutstanding.add(_profit);
 
-        if (amountToFree > 0) {
+        if (amountToFree > 0 && balanceOfWant() < amountToFree) {
             uint256 amountFreed = 0;
             (amountFreed, _loss) = liquidatePosition(amountToFree);
-            _debtPayment = Math.min(amountFreed.sub(_profit), _debtOutstanding);
+
+            // if I cannot free enough, or just enough, then it's all profit ser
+            if (_profit >= amountFreed) {
+                _profit = amountFreed;
+                _debtPayment = 0;
+            } else {
+                // I have more than enough, some is profit and the rest is debt repayment
+                _debtPayment = Math.min(
+                    amountFreed.sub(_profit),
+                    _debtOutstanding
+                );
+            }
         }
     }
 
@@ -155,16 +165,19 @@ contract Strategy is BaseStrategy {
         uint256 balanceOfWantNow = balanceOfWant();
 
         if (_amountNeeded > balanceOfWantNow) {
-            _liquidatedAmount = Math.min(
-                balanceStaked(),
-                _amountNeeded.sub(balanceOfWantNow)
-            );
-            mph88Rewards.withdraw(_liquidatedAmount);
+            uint256 amountToUnstake =
+                Math.min(balanceStaked(), _amountNeeded.sub(balanceOfWantNow));
+            mph88Rewards.withdraw(amountToUnstake);
+
+            _liquidatedAmount = amountToUnstake.add(balanceOfWantNow);
+
+            if (_amountNeeded > _liquidatedAmount) {
+                _loss = _liquidatedAmount.sub(_amountNeeded);
+            }
         } else {
             _liquidatedAmount = _amountNeeded;
+            _loss = 0;
         }
-
-        _loss = balanceOfWant().sub(_amountNeeded);
     }
 
     function liquidateAllPositions() internal override returns (uint256) {
